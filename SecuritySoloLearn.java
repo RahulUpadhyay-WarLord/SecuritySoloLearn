c04a7b4f28726a0e2c7c60953777ab102310b805
luwie <lukwie@gmail.com>
2020-04-04 22:10:06 +0200

    add substring method

---
+++ SecuritySoloLearn.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SecuritySoloLearn {
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println(input);
+		char [] signs = input.toCharArray();
+		int money = input.indexOf('$');
+		int thief = input.indexOf('T');
+		System.out.println("money: " + money + ", thief: " + thief);
+		String sub = subStr(input, money, thief);
+		System.out.println(sub);
+
+	}
+
+	static String subStr(String in, int mon, int thi) {
+		String ret = "";
+		if (mon < thi) {
+			ret = in.substring(mon+1,thi);
+		} else {
+			ret = in.substring(thi+1,mon);
+		}
+		return ret;
+	}
+}
